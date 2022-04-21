d2541f921fed62f018bc2c8ded38c4562a3cdce2
Nur Maulana Rizqi <101783236+nurmaulana12@users.noreply.github.com>
2022-04-21 09:59:45 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class contoh1 extends CI_controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama Nur Maulana rizqi
+        saya tinggal di daerah tangerang
+        olahraga yang saya suka adalah voli";
+
+    }
+}
